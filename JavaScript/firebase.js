5efcbd9c4b8414ca028d7653e2490bda2d388bea
tecnico54 <115370228+tecnico54@users.noreply.github.com>
2025-11-01 01:43:41 -0500

    Create firebase.js

---
+++ JavaScript/firebase.js
@@ -0,0 +1,19 @@
+// --- Importaciones ---
+const admin = require("firebase-admin");
+const serviceAccount = require("../JSON/serviceAccountKey.json");
+
+// --- Inicialización de Firebase ---
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  // Cambia esta URL por la de tu proyecto Firestore real (de la pestaña "Configuración del proyecto")
+  databaseURL: "https://novedad-del-bolso.firebaseio.com"
+});
+
+// --- Inicialización de Firestore ---
+const db = admin.firestore();
+
+// (Opcional pero recomendable): configuración de timestamps
+db.settings({ ignoreUndefinedProperties: true });
+
+// --- Exportación ---
+module.exports = db;
